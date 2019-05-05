e126fdaeb0d6b9f4539e6bb6fd1918db5cca8b14
Shahid <wizshahid@gmail.com>
2019-05-05 11:55:09 +0530

    Added Student Controller

--- GitDemo/Controllers/StudentController.cs
+++ GitDemo/Controllers/StudentController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using GitDemo.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,13 +10,37 @@ namespace GitDemo.Controllers
     public class StudentController : Controller
     {
         // GET: Student
+        GitDemoDbEntities db = new GitDemoDbEntities();
         public ActionResult Index()
         {
             return View();
         }
         public ActionResult AddStudent()
         {
+            ViewBag.DepartmentId = new SelectList(db.Departments,"Id", "DeptName");
             return View();
         }
+        [HttpPost]
+        public ActionResult AddStudent(Student stud)
+        {
+            db.Students.Add(stud);
+            db.SaveChanges();
+            ViewBag.DepartmentId = new SelectList(db.Departments, "Id", "DeptName");
+            return View();
+        }
+
+        public ActionResult AddDepartment()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddDepartment(Department department)
+        {
+            db.Departments.Add(department);
+            db.SaveChanges();
+            return View();
+        }
+
     }
 }
---
+++ GitDemo/Models/GitDemoDbEntities.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace GitDemo.Models
+{
+    public class GitDemoDbEntities:DbContext
+    {
+        public GitDemoDbEntities():base ("GitDemoDbEntities")
+            {
+            }
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Department> Departments { get; set; }
+
+    }
+}
